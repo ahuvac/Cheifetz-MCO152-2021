81f56f0c0d95749ce9106c1a9a3a26a8d27a859e
ahuvac <58051262+ahuvac@users.noreply.github.com>
2021-02-03 13:56:55 -0500

    fixed variables

--- src/testRocket.java
+++ src/testRocket.java
@@ -13,13 +13,13 @@ public class testRocket {
     @Test
     public void getY() {
         Rocket rocket = new Rocket(71, 35);
-        double x = rocket.getY(3);
-        Assert.assertEquals(78.07, x, 0.01);
+        double y = rocket.getY(3);
+        Assert.assertEquals(78.07, y, 0.01);
     }
     @Test
     public void TimeToLand() {
         Rocket rocket = new Rocket(71, 35);
-        double x = rocket.timeToLand();
-        Assert.assertEquals(8.31, x, 0.01);
+        double time = rocket.timeToLand();
+        Assert.assertEquals(8.31, time, 0.01);
     }
 }
